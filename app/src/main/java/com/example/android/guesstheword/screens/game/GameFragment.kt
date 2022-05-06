8c7e23058ae495750f1b330129eacfbdbd847400
kieubaongoc <tt.ngoc.kieu@tomosia.com>
2022-05-06 16:38:57 +0700

    Implement click listener for the End Game button

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -21,9 +21,11 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.NavHostFragment
 import com.example.android.guesstheword.R
 import com.example.android.guesstheword.databinding.GameFragmentBinding
 
@@ -58,6 +60,10 @@ class GameFragment : Fragment() {
         binding.skipButton.setOnClickListener { onSkip() }
         updateScoreText()
         updateWordText()
+
+        // Set a click listener for the End Game button using dataBinding
+        binding.endGameButton.setOnClickListener { onEndGame() }
+
         return binding.root
 
     }
@@ -84,5 +90,21 @@ class GameFragment : Fragment() {
 
     private fun updateScoreText() {
         binding.scoreText.text = viewModel.score.toString()
+    }
+
+    //  Add a method gameFinished() to navigate the app to the score screen. Pass in the score as an argument, using Safe Args.
+    /**
+     * Called when the game is finished
+     */
+    private fun gameFinished() {
+        Toast.makeText(activity, "Game has just finished", Toast.LENGTH_SHORT).show()
+        val action = GameFragmentDirections.actionGameToScore()
+        action.score = viewModel.score
+        NavHostFragment.findNavController(this).navigate(action)
+    }
+
+    private fun onEndGame() {
+        // Call gameFinished()
+        gameFinished()
     }
 }
